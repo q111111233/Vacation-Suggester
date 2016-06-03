39a64b1519c3e86b2fcf845abbb64e35fdc5f032
Bo Zhao <zhaobo419@gmail.com>
2016-06-03 15:15:04 -0700

    ask the user for their name and  identify the letter that was most frequently selected

--- js/script.js
+++ js/script.js
@@ -6,23 +6,49 @@ $(document).ready(function(){
     var who = $("#who").val();
     var long = $("#long").val();
     var enviroment = $("#enviroment").val();
-    var place;
+    var place = mostChosen(continent, like, who, long, enviroment);
 
-    if(continent === "Asia" && like === "Adventure" && who === "Travel Group" && long === "Over three weeks" && enviroment === "Polar"){
-      place = "Mount Everest";
+    if(name===""){
+      $(".name").text("Please enter your name");
     }
-    else if(enviroment === "Outter space"){
-      place = "Mars";
-    }
-    else if(long === "A few hours"){
-      place = "Dream";
-    }
-    else {
-      place = "Home";
+    else{
+      $(".name").text(name);
+      if(continent===null || like===null || who===null || long===null || enviroment===null){
+        $(".place").text("Please choose your preference");
+      }
+      else{
+        $(".place").text(place);
+      }
     }
 
-    $(".place").text(name + " " + place);
     $("#result").fadeIn(3000);
     event.preventDefault();
   });
 });
+
+// a funtion to find the most frequently selected option
+var mostChosen = function(continent, like, who, long, enviroment){
+  var store = [continent, like, who, long, enviroment];
+  var frequency = {};
+  var max = 0;
+  var result = 0;
+  for(var v in store) {
+    frequency[store[v]]=(frequency[store[v]] || 0)+1;
+    if(frequency[store[v]] > max) {
+      max = frequency[store[v]];
+      result = store[v];
+    }
+  }
+  if(result === "a"){
+    return "you chose mostly a's, you should get ready to climb on Mount Everest!";
+  }
+  else if(result === "b"){
+    return "you chose mostly b's, you should experience ancient culture in Egypt!";
+  }
+  else if(result === "c"){
+    return "you chose mostly c's, you should enjoy beach and sunshine in Rome!";
+  }
+  else if(result === "d"){
+    return "you chose mostly d's, you should stay in bed and have a nice dream!";
+  }
+}
